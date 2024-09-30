cd89e198101fafca3c2f12e0f721a3fa510752cb
Max Dahlbom <max@qasa.se>
2024-09-30 17:10:03 +0200

    Clean things up

--- src/main.js
+++ src/main.js
@@ -1,29 +1,29 @@
 const BAD_WORDS = ["ful", "dum"];
-const config = { attributes: true, subtree: true, childList: true };
+const TAGS = ["p", "span", "label"];
+const CONFIG = { attributes: true, subtree: true, childList: true };
+
+console.log("Profanity Poof is running...");
 
 const callback = (mutationList, observer) => {
   for (const mutation of mutationList) {
     if (mutation.type === "childList") {
-      replaceInnerTextInTag("p");
-      replaceInnerTextInTag("span");
-      console.log("A child node has been added or removed.");
-    } else if (mutation.type === "attributes") {
-      return;
-      console.log(`The ${mutation.attributeName} attribute was modified.`);
+      replaceInnerTextInTags();
     }
   }
 };
 
 const observer = new MutationObserver(callback);
-observer.observe(document.body, config);
+observer.observe(document.body, CONFIG);
 
-function replaceInnerTextInTag(tagName) {
-  const tags = document.querySelectorAll(tagName);
+function replaceInnerTextInTags() {
+  const tags = document.querySelectorAll(TAGS);
   const badWordRegex = new RegExp(`\\b(${BAD_WORDS.join("|")})\\b`, "gi");
 
   tags.forEach((tag) => {
-    if (tag.hasAttribute("data-censored")) return;
-    if (badWordRegex.test(tag.innerText)) {
+    if (
+      badWordRegex.test(tag.innerText) &&
+      !tag.hasAttribute("data-censored")
+    ) {
       tag.setAttribute("data-censored", "");
       tag.innerText = tag.innerText.replace(badWordRegex, "!@)%)@");
     }
