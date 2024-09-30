d48342c47d8cd876c53b29d369723bcec4200acd
Max Dahlbom <max@qasa.se>
2024-09-30 14:23:43 +0200

    Initial sort of working version

---
+++ src/main.js
@@ -0,0 +1,27 @@
+const BAD_WORDS = ["ful", "dum"];
+const config = { attributes: true, subtree: true, childList: true };
+
+const callback = (mutationList, observer) => {
+  for (const mutation of mutationList) {
+    if (mutation.type === "childList") {
+      replaceInnerTextInTag("p");
+      console.log("A child node has been added or removed.");
+    } else if (mutation.type === "attributes") {
+      console.log(`The ${mutation.attributeName} attribute was modified.`);
+    }
+  }
+};
+
+const observer = new MutationObserver(callback);
+observer.observe(document.body, config);
+
+function replaceInnerTextInTag(tagName) {
+  const tags = document.querySelectorAll(tagName);
+  tags.forEach((tag) => {
+    if (!BAD_WORDS.some((word) => tag.innerText.includes(word))) return;
+    const wordsInInnerText = tag.innerText.split(" ");
+    BAD_WORDS.forEach(
+      (word) => (tag.innerText = tag.innerText.replaceAll(word, "!@)%)@")),
+    );
+  });
+}
