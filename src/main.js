2ac4ee5e55a8c088345ca5b9404d97797797242f
Max Dahlbom <max@qasa.se>
2024-09-30 17:02:30 +0200

    Add zendesk sandbox, check attribute

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ const callback = (mutationList, observer) => {
   for (const mutation of mutationList) {
     if (mutation.type === "childList") {
       replaceInnerTextInTag("p");
+      replaceInnerTextInTag("span");
       console.log("A child node has been added or removed.");
     } else if (mutation.type === "attributes") {
       return;
@@ -21,7 +22,9 @@ function replaceInnerTextInTag(tagName) {
   const badWordRegex = new RegExp(`\\b(${BAD_WORDS.join("|")})\\b`, "gi");
 
   tags.forEach((tag) => {
+    if (tag.hasAttribute("data-censored")) return;
     if (badWordRegex.test(tag.innerText)) {
+      tag.setAttribute("data-censored", "");
       tag.innerText = tag.innerText.replace(badWordRegex, "!@)%)@");
     }
   });
