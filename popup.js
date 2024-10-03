ed1725095a3168d6991855b19b104a653b7fdcea
Max Dahlbom <73931682+max-da@users.noreply.github.com>
2024-10-03 15:58:37 +0200

    Implement api (#1)
    * Implement api
    
    * Cleanup
    
    * Prevent default etc
    
    * Search wokrs

--- popup.js
+++ popup.js
@@ -1,112 +1,56 @@
-// var words = require("./words");
-// import {words} from './words.js';
+let words = [];
 
-var words = [
-	"luder",
-	"röv",
-	"satan"
-];
+const wordInputField = document.getElementById("wordInput");
+const wordList = document.getElementById("wordList");
+
+wordInputField.addEventListener("input", (e) => {
+  wordList.innerText = "";
+  createListItems(e.target.value);
+});
 
 function addWord(wordInput) {
-
-	if (wordInput === '') {
-		return;
-	}
-
-	words.push(wordInput);
+  if (wordInput === "") {
+    return;
+  }
+  words.push(wordInput);
 }
 
-function removeWord(event) {
-	return;
+function createListItem(word) {
+  const listItem = document.createElement("li");
+  const listWordText = document.createElement("span");
+
+  listWordText.textContent = word;
+  listItem.appendChild(listWordText);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "x";
+  listItem.appendChild(deleteButton);
+  return listItem;
 }
 
-function listWords() {
-	words.map(function (word) {
-		const listItem = document.createElement('li');
-		const listWordText = document.createElement('span');
-		listWordText.textContent = word;
-		listItem.appendChild(listWordText);
+function createListItems(input) {
+  if (!words.length) {
+    return null;
+  }
+  const filteredWords = words.filter((word) => {
+    if (!word.title) {
+      return false;
+    }
+    return word.title.toLowerCase().startsWith(input.toLowerCase());
+  });
 
-		const deleteButton = document.createElement('button');
-		deleteButton.textContent = 'x';
-		listItem.appendChild(deleteButton);
-
-		document.getElementById('wordList').appendChild(listItem);
-
-	})
+  filteredWords.map((word) => {
+    wordList.appendChild(createListItem(word.title));
+  });
 }
 
-var wordInputField = document.getElementById('wordInput')
-
-document.getElementById('addWord').addEventListener('click', function () {
-	addWord(wordInput.value.trim());
-	wordInput.value = '';
-	listWords();
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
 });
-//document.getElementById('removeWord').addEventListener('click', removeWord);
 
-
-
-
-
-
-
-// function addWord(word) {
-// 	if (word === '') {
-// 		//document.getElementById('wordInput') = '';  vi gör det utanför
-// 		return;
-// 	}
-
-// 	const listItem = document.createElement('li');
-// 	const listWordText = document.createElement('span');
-// 	listWordText.textContent = word;
-// 	console.log(word);
-// 	listItem.appendChild(listWordText);
-
-// 	const deleteButton = document.createElement('button');
-// 	deleteButton.textContent = 'x';
-// 	listItem.appendChild(deleteButton);
-
-// 	wordList.appendChild(listItem);
-
-
-
-// 	deleteButton.addEventListener('click', function () {
-// 		wordList.removeChild(listItem);
-// 	});
-
-// 	saveTasksToLocalStorage();
-// }
-
-// const wordList = document.getElementById('wordList');
-// const wordInput = document.getElementById('wordInput'); //passar det att det är en const?
-
-// document.getElementById('wordsForm').addEventListener('submit', function (event) {
-// 	addWord(wordInput.value);
-// 	wordInput.value = '';
-// });
-
-// function saveTasksToLocalStorage() {
-// 	const words = [];
-// 	document.querySelectorAll('#wordList li').forEach(wordLi => {
-// 		const word = wordLi.querySelector('span').textContent;
-// 		words.push(word);
-// 	});
-// 	localStorage.setItem('words', JSON.stringify(words));
-// 	console.log(words);
-// }
-
-// window.onload = function () {
-// 	const savedWords = JSON.parse(localStorage.getItem('words')) || [];
-// 	savedWords.forEach(word => {
-// 		addWord(word);
-// 	});
-// };
-
-// document.addEventListener('DOMContentLoaded', function () {
-// 	const savedWords = JSON.parse(localStorage.getItem('words')) || [];
-// 	savedWords.forEach(word => {
-// 		console.log(word);
-// 		addWord(word.text);
-// 	});
-// });
+window.onload = () => {
+  console.log("hej");
+  chrome.runtime.sendMessage({ action: "getCurseWords" }).then(({ items }) => {
+    words = items;
+  });
+};
